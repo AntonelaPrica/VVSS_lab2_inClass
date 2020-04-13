d88d27744b5f69a561becccef9f242eabf326363
Antonela Prica <pricaantonela@yahoo.com>
2020-04-13 17:27:12 +0300

    WBTs for addAssignment

--- src/test/java/VVSS_lab2/WhiteBoxTest.java
+++ src/test/java/VVSS_lab2/WhiteBoxTest.java
@@ -1,10 +1,8 @@
 package VVSS_lab2;
 
-import VVSS_lab2.Domain.Student;
 import VVSS_lab2.Domain.TemaLab;
 import VVSS_lab2.Exceptions.ValidatorException;
 import VVSS_lab2.Repository.MemoryRepository.TemaLabRepo;
-import VVSS_lab2.Service.TxtFileService.TemaLabService;
 import VVSS_lab2.Validator.TemaLabValidator;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +15,7 @@ public class WhiteBoxTest {
     private TemaLabValidator temaLabValidator;
 
     @Before
-    public void setUp()
-    {
+    public void setUp() {
         temaLabValidator = new TemaLabValidator();
         temaLabRepo = new TemaLabRepo(temaLabValidator);
     }
@@ -26,8 +23,8 @@ public class WhiteBoxTest {
     @Test
     public void addAssignment_TC1() {
 
-        TemaLab temaLab1 = new TemaLab(1,"first homework",5,4);
-        TemaLab temaLab2 = new TemaLab(1,"second homework",7,5);
+        TemaLab temaLab1 = new TemaLab(1, "first homework", 5, 4);
+        TemaLab temaLab2 = new TemaLab(1, "second homework", 7, 5);
 
         try {
             temaLabRepo.save(temaLab1);
@@ -43,13 +40,40 @@ public class WhiteBoxTest {
     }
 
     @Test
-    public void addAssignment_TC2(){
+    public void addAssignment_TC2() {
         try {
             temaLabRepo.save(null);
             fail(); // null object should not be added
 
         } catch (Exception e) {
             assertEquals(temaLabRepo.size(), 0);
+            assertEquals(e.getMessage(), "Entity can not be null!\n");
+        }
+    }
+
+    @Test
+    public void addAssignment_TC3() {
+        TemaLab temaLab = new TemaLab(1, "Lab Homework", 8, 7);
+        try {
+            TemaLab prevValue = temaLabRepo.save(temaLab);
+            assertEquals(temaLabRepo.size(), 1);
+            assertNull(prevValue);
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void addAssignment_TC4() {
+        TemaLab temaLab = new TemaLab(2, "", 5, 8);
+        try {
+            temaLabRepo.save(temaLab);
+            fail();
+
+        } catch (Exception e) {
+            assertEquals(temaLabRepo.size(), 0);
+            assertEquals(e.getMessage(), "Descriere tema invalida\n");
         }
     }
 }

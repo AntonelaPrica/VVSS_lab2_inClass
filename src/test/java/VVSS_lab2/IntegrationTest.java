2afbb94ed3621a8ba11463fa5af0e7a3313edda5
Antonela Prica <pricaantonela@yahoo.com>
2020-04-15 17:53:08 +0300

    Incremental integration

--- src/test/java/VVSS_lab2/IntegrationTest.java
+++ src/test/java/VVSS_lab2/IntegrationTest.java
@@ -14,8 +14,7 @@ import VVSS_lab2.Validator.TemaLabValidator;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class IntegrationTest {
 
@@ -41,49 +40,87 @@ public class IntegrationTest {
     }
 
     @Test
-    public void add_TC1()
-    {
+    public void add_TC1() {
         Student student1 = new Student("1", "Stud1", 10, "stud1@test.com", "Prof");
         try {
             studentRepo.save(student1);
             assertEquals(studentRepo.size(), 1);
-        }catch (ValidatorException v)
-        {
+        } catch (ValidatorException v) {
             fail();
         }
     }
 
     @Test
-    public void add_TC2()
-    {
+    public void add_TC2() {
         TemaLab temaLab1 = new TemaLab(1, "first homework", 5, 4);
         try {
             temaLabRepo.save(temaLab1);
             assertEquals(temaLabRepo.size(), 1);
-        }catch (ValidatorException v)
-        {
+        } catch (ValidatorException v) {
             fail();
         }
     }
 
     @Test
-    public void add_TC3()
-    {
-        Nota nota = new Nota(1,"1",1,10);
+    public void add_TC3() {
+        Nota nota = new Nota(1, "1", 1, 10);
         try {
             notaRepo.save(nota);
             assertEquals(notaRepo.size(), 1);
-        }catch (ValidatorException v)
-        {
+        } catch (ValidatorException v) {
             fail();
         }
     }
 
     @Test
-    public void add_TC4()
-    {
+    public void add_TC4() {
         add_TC1();
         add_TC2();
         add_TC3();
     }
+
+    @Test
+    public void incr_TC_addStud() {
+        studentRepo = new StudentRepo(studentValidator);
+        Student stud = new Student("1", "Rus Mihai", 1, "rm@yahoo.com", "Prof. Maria");
+        try {
+            studentRepo.save(stud);
+        } catch (ValidatorException e) {
+            assertEquals(studentRepo.size(), 0);
+        }
+        assertEquals(studentRepo.size(), 1);
+    }
+
+    @Test
+    public void incr_TC_addAssign() {
+
+        incr_TC_addStud(); // test add student
+        temaLabRepo = new TemaLabRepo(temaLabValidator);
+        TemaLab temaLab = new TemaLab(1, "Lab Homework", 8, 7);
+        try {
+            TemaLab prevValue = temaLabRepo.save(temaLab);
+            assertEquals(temaLabRepo.size(), 1);
+            assertNull(prevValue);
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void incr_TC_addGrade() {
+
+        incr_TC_addStud(); // test add student
+        add_TC2(); //  test add assignment
+
+        notaRepo = new NotaRepo(notaValidator);
+        Nota nota = new Nota(1, "1", 1, 10);
+        try {
+            notaRepo.save(nota);
+            assertEquals(notaRepo.size(), 1);
+        } catch (ValidatorException v) {
+            fail();
+        }
+    }
+
 }
